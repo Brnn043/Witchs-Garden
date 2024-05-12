9f47a0eac18c7713844d53149d65e6cfba80de17
neennera <neennera@gmail.com>
2024-05-12 09:58:38 +0700

    add: stackPane1

--- src/GUI/PreStory.java
+++ src/GUI/PreStory.java
@@ -1,11 +1,14 @@
 package GUI;
 
+import Games.Config;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -16,6 +19,7 @@ public class PreStory extends GridPane {
     private ArrayList<String> analogs;
     private int count;
     private Text analog;
+    private StackPane backgroundImage;
     @FunctionalInterface
     public interface GameStarter {
         void startGame();
@@ -26,29 +30,10 @@ public class PreStory extends GridPane {
         analogs = new ArrayList<String>();
         analogs.add("Once upon a time, in a forest far, far away, there lived a witch with extraordinary powers.");
         analogs.add("One morning, she woke up to a surprising discovery - her magic had disappeared!");
-        analogs.add("Feeling sad, she thought");
-        analogs.add("\"Hmm, maybe I can make three magic potions from special veggies to get my power back\"");
+        analogs.add("Feeling sad, she thought \"Hmm, maybe I can make three magic potions from special veggies to get my power back\"");
         analogs.add("Off she went to her garden. But unexpectedly , her garden was full of slimy slimes!");
         analogs.add("She didn't give up! With a determined smile, she started her journey!");
-
-        //set default background image
-        Image image = new Image(ClassLoader.getSystemResource("MenuPage/Sunny_MenuPage.gif").toString());
-        BackgroundImage backgroundImage = new BackgroundImage(
-                image,
-                BackgroundRepeat.NO_REPEAT,   // Repeat settings
-                BackgroundRepeat.NO_REPEAT,
-                javafx.scene.layout.BackgroundPosition.DEFAULT,
-                new BackgroundSize(
-                        BackgroundSize.AUTO,  // Width
-                        BackgroundSize.AUTO,  // Height
-                        false,               // Preserve ratio
-                        false,               // Smooth
-                        true,                // Resize to fit
-                        false                // Repeat vertically
-                )
-        );
-        Background background = new Background(backgroundImage);
-        this.setBackground(background);
+        backgroundImage = new StackPane();
 
         Button nextButton = new Button("Next...");
         nextButton.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 18));
@@ -83,15 +68,29 @@ public class PreStory extends GridPane {
         textArea.setBackground(new Background(new BackgroundFill(Color.rgb(255, 153, 204, 0.9), new CornerRadii(8), null)));
 
         this.setAlignment(Pos.BOTTOM_LEFT);
-        this.add(textArea, 0, 0);
-        this.add(nextButton, 2, 0);
+        this.add(textArea, 0, 2);
+        this.add(nextButton, 2, 2);
+        this.add(backgroundImage,0,1,2,1);
         this.setPadding(new Insets(20));
         this.setHgap(10);
+        this.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+
+
+
         showLog(count);
     }
 
     public void showLog(int logCount){
         analog.setText(analogs.get(logCount));
+
+        //set default background image
+        Image image = new Image(ClassLoader.getSystemResource("Story/BeginStory_" + Integer.toString(logCount+1) + ".png").toString());
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(700);
+        imageView.setFitWidth(1100);
+        Rectangle overlay = new Rectangle(1100, 500, Color.BLACK);
+        backgroundImage.getChildren().addAll(overlay, imageView);
+
         count = count + 1;
     }
 }
