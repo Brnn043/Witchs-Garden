b462b201494449f6dc797b635e15ce84d50b1a29
neennera <neennera@gmail.com>
2024-05-13 15:15:57 +0700

    change: change analogs

--- src/GUI/Scene/PreStory.java
+++ src/GUI/Scene/PreStory.java
@@ -17,7 +17,7 @@ import javafx.scene.text.Text;
 import java.util.ArrayList;
 
 public class PreStory extends GridPane {
-    private ArrayList<String> analogs;
+    private ArrayList<String> ANALOGS;
     private int count;
     private Text analog;
     private ImageView backgroundImage;
@@ -28,12 +28,12 @@ public class PreStory extends GridPane {
     public PreStory(GameStarter gameStarter) {
         count = 0;
         analog = new Text();
-        analogs = new ArrayList<>();
-        analogs.add("Once upon a time, in a forest far, far away, there lived a witch with extraordinary powers.");
-        analogs.add("One morning, she woke up to a surprising discovery - her magic had disappeared!");
-        analogs.add("She thought \"Hmm, maybe I can make three magic potions from special veggies to get my power back\"");
-        analogs.add("Off she went to her garden. But unexpectedly , her garden was full of slimy slimes!");
-        analogs.add("She didn't give up! With a determined smile, she started her journey!");
+        ANALOGS = new ArrayList<>();
+        ANALOGS.add("Once upon a time, in a forest far, far away, there lived a witch with extraordinary powers.");
+        ANALOGS.add("One morning, she woke up to a surprising discovery - her magic had disappeared!");
+        ANALOGS.add("She thought \"Hmm, maybe I can make three magic potions from special veggies to get my power back\"");
+        ANALOGS.add("Off she went to her garden. But unexpectedly , her garden was full of slimy slimes!");
+        ANALOGS.add("She didn't give up! With a determined smile, she started her journey!");
         backgroundImage =  new ImageView();
 
         Button nextButton = new Button("Next...");
@@ -49,7 +49,7 @@ public class PreStory extends GridPane {
             nextButton.setBackground(new Background(new BackgroundFill(Color.valueOf("#8F6F5C"), new CornerRadii(8), null)));
         });
         nextButton.setOnAction(e -> {
-            if(count == analogs.size()){
+            if(count == ANALOGS.size()){
                 gameStarter.startGame();
             }else{
                 showLog(count);
@@ -84,7 +84,7 @@ public class PreStory extends GridPane {
     }
 
     public void showLog(int logCount){
-        analog.setText(analogs.get(logCount));
+        analog.setText(ANALOGS.get(logCount));
 
         //set default background image
         Image image = new Image(ClassLoader.getSystemResource("Story/BeginStory_" + Integer.toString(logCount+1) + ".png").toString());
